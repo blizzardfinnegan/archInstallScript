e4783a24386b4853d98d0202385c1395d7d59ab0
Blizzard Finnegan <blizzardfinnegan@gmail.com>
2021-10-23 17:21:09 -0400

    Add disk partitioning

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Generalized arch install script
 # NOTE: This script will not work on older BIOS boot computers. 
+# NOTE: This script assumes you already have networking access, and that it is being run on x86-64 hardware.
 
 #Error-catching: Any error will cancel out of the script, and tell you what line caused the crash
 set -e
@@ -48,7 +49,8 @@ then
     keymapSet=0
     while [keymapSet -eq 0]
     do
-        read -p 'Enter the two-letter code for the language of your keyboard. (If English, enter the keyboard style you will use [i.e. dvorak]): ' keymapSearch
+        echo Enter the two-letter code for the language of your keyboard. 
+        read -p '(If English, enter the keyboard style you will use [i.e. dvorak]): ' keymapSearch
         localectl list-keymaps | grep -i $keymapSearch
         read -p 'Enter the correct code for your keyboard, or if not available, leave blank to search again: ' keymap
         if [-n "$keymap"]
@@ -71,13 +73,57 @@ ls /sys/firmware/efi/efivars
 ### -----------------------
 #   Update System Clock
 ### -----------------------
+timedatectl set-ntp true
 
 ### -----------------------
 #   Partition Disk
 ### -----------------------
-# Notes for later
-# lsblk | grep -v '-' | awk -v OFS='\t' '{pring $1, $4}'
-# This prints the major disks, and their storage sizes.
+echo Do you want to use the default disk partition scheme?
+echo Default partition scheme:
+echo - 512MB FAT32 boot partition  \(Boot partition must be in FAT32\)
+echo - the remaining space of the drive \(File system type will be set later \)
+read -p 'If you are unsure what this means, say yes (Y/n): ' defaultPartition
+if ["$defaultPartition" == "n" -o "$defaultPartition" == "N"]
+then
+    echo From here, I assume you know what you are doing.
+    echo I will temporarily drop you back into the shell. When you are done partitioning, exit the shell to continue.
+    bash
+else
+    lsblk | grep -v '-' | grep -v 'rom'| awk -v OFS='\t' '{pring $1, $4}'
+    # This prints the major disks, and their storage sizes.
+    # The grep portion of the command removes all lines that have a '-' in it
+    #   This leaves us only with high-level drive names.
+    # The second grep command removes 'rom' devices (read as: DVD/Blu-Ray drives)
+    # The awk command only prints the first and 4th columns (name and size). 
+    #   The -v OFS changes the Output Field Separator to be "\t", the escape character for Tab
+    read -p 'Enter a drive to be partitioned.' drivePart1
+    fdisk /dev/$drivePart1 -W always<<EOF
+g
+n
+1
+
++512M
+n
+2
+
+
+t
+1
+1
+t
+2
+23
+w
+EOF
+fi
+## Explanation of fdisk commands:
+# --------------------------------
+# g creates a GPT partition table
+# The first n, and all lines up to the next n, creates a new partition, with size 512MB
+# The second n, and all lines to the t, create a second partition, taking up the rest of the drive.
+# the first t, and following digits, labels the first partition as a boot partition.
+# The second t, and the following digits, labels the second partition as a Linux root partition
+# The w writes out the changes in memory to the disk.
 
 ### -----------------------
 #   Format Partitions
